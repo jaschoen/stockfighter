3789fe6221580353b40d22d24718443e03118466
Brandon Keao <brandon.keao@gmail.com>
2016-04-12 14:29:54 -0500

    Minor updates to API class

--- main_wrapper.rb
+++ main_wrapper.rb
@@ -4,22 +4,14 @@ require_relative 'LevelInfo'
 require_relative 'level_one'
 
 this_key = StockfighterAPI.new
-this_level = LevelInfo.new(1.to_s, "FKSEX", "PEIM", "SW14934361")
-
-this_level_one = LevelOne.new(1.to_s, "FKSEX", "PEIM", "SW14934361")
 
 puts "\nREADY TRADER ONE...\n\n"
-puts "Initialized with API KEY: " + this_key.api_key
-puts "Target API URL: " + this_key.base_url
 
-puts "Level: " + this_level_one.level
-puts "Venue: " + this_level_one.venue
-puts "Stock: " + this_level_one.stock
-puts "Account: " + this_level_one.account
+puts "IS API UP?? : " + this_key.isApiUp.to_s
+puts "Reponse after isApiUp test: " + this_key.response.to_s
 
-this_key.isApiUp
-if this_key.IsVenueUp(this_level_one.venue)
-	puts "SUCCESS: " + this_level_one.venue
-else
-	puts "FAIL: " + this_level_one.venue
-end
+puts "IS VENUE UP?? : " + this_key.isVenueUp("TESTEX").to_s
+puts "Reponse after isApiUp test: " + this_key.response.to_s
+
+puts "IS VENUE UP?? : " + this_key.isVenueUp("TEEX").to_s
+puts "Reponse after isApiUp test: " + this_key.response.to_s
